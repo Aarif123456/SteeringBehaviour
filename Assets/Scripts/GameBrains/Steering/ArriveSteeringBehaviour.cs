06718f880bef94ee80ee247ba2368dab757ff030
Abdullah Arif <arif115@uwindsor.ca>
2021-03-01 23:18:17 -0500

    fixed arrival

--- Assets/Scripts/GameBrains/Steering/ArriveSteeringBehaviour.cs
+++ Assets/Scripts/GameBrains/Steering/ArriveSteeringBehaviour.cs
@@ -4,58 +4,21 @@ using UnityEngine;
 // Add to the component menu.
 namespace GameBrains.Steering
 {
-    public enum Deceleration{
-        Slow=3,
-        Normal=2,
-        Fast=1
-    }
-
+    
     [AddComponentMenu("Scripts/Steering/Arrive")]
    
     // Require a motor to be attached to the parent game object.
     [RequireComponent(typeof(Motor))]
 
-    public class ArriveSteeringBehaviour : SteeringBehaviour
+    public class ArriveSteeringBehaviour : SeekSteeringBehaviour
     {
-        public const float DECELERATION_TWEAKER = 0.3f;
-        public Deceleration decelerationMode = Deceleration.Fast;
+        public float slowingRadius = 5f;
+
         protected override void DetermineDesiredDirection()
         {
-            if (motor != null && motor.isAiControlled)
-            {
-                if (targetObject != null)
-                {
-                    targetPosition = targetObject.transform.position;
-                }
-
-                // vector from current to target position.
-                desiredMoveDirection = targetPosition - transform.position;
-                desiredMoveDirection.y = 0;
-
-                // Get the length of the direction vector which is the distance to the target.
-                distanceToTarget = desiredMoveDirection.magnitude;
-
-                // Reduce speed based on how close we are to target 
-                desiredMoveDirection *= (distanceToTarget/((float)decelerationMode * DECELERATION_TWEAKER));
-
-                // if not there yet ...
-                if (distanceToTarget > satisfactionRadius)
-                {
-                    // Dividing by the length is cheaper than normalizing when we already have the length anyway
-                    desiredMoveDirection /= distanceToTarget;
-
-                    // Multiply the normalized direction vector by the distance capped at 1.
-                    desiredMoveDirection *= Mathf.Min(1, distanceToTarget);
-                }
-                else
-                {
-                    // we're there (close enough). Stop.
-                    desiredMoveDirection = Vector3.zero;
-                }
-            }
-            else
-            {
-                desiredMoveDirection = Vector3.zero;
+            base.DetermineDesiredDirection();
+            if (distanceToTarget < slowingRadius) {
+                desiredMoveDirection *= distanceToTarget / slowingRadius;
             }
         }
     }
